0cce284fa057fc6c760ba34c5a89b78032faaa66
Francisco Pinho <up201303744@fe.up.pt>
2019-03-01 17:25:52 +0000

    changing 360 environments successfully

---
+++ components/navigation.js
@@ -0,0 +1,53 @@
+AFRAME.registerComponent('navigation', {
+    schema:{
+        origin: {type:'selector'},
+        destination: {type:'selector'},
+        dur: {type:'number', default: 500}
+    },
+    init:  function () {
+        this.setFadeInOrOut = this.setFadeInOrOut.bind(this);
+        var data = this.data;
+        var origin = data.origin;
+        var destination = data.destination;
+
+        this.setFadeInOrOut('out',origin);
+        this.setFadeInOrOut('in',destination);
+        this.el.addEventListener('click', function () {
+            // Fade out image.
+            origin.emit('set-image-fade-out');
+            // Wait for fade to complete.
+            setTimeout(function () {
+            // Set image.
+            origin.setAttribute('visible',false)
+            destination.setAttribute('visible',true)
+            destination.emit('set-image-fade-in');
+            }, data.dur);
+        });
+       
+    },
+
+    /**
+     * Setup fade-in + fade-out.
+     */
+    setFadeInOrOut: function (direction,targetEl) {
+        // Only set up once.
+        if(direction==="in"){
+            if (targetEl.dataset.setImageFadeInSetup) { return; }
+            targetEl.dataset.setImageFadeInSetup = true;
+        }
+        else{
+            if (targetEl.dataset.setImageFadeOutSetup) { return; }
+            targetEl.dataset.setImageFadeOutSetup = true;
+        }
+        // Create animation.
+        targetEl.setAttribute('animation__fade'+'__'+direction, {
+        property: 'material.color',
+        startEvents: 'set-image-fade-'+direction,
+        dir: direction==='out'?'normal':'reverse',
+        dur: this.data.dur,
+        from: '#FFF',
+        to: '#000'
+        });
+    },
+
+  });
