27038e161d6bba99cff31c257b74ed68d3b96b7c
FranciscoPinho <up201303744@fe.up.pt>
2019-03-11 16:28:39 +0000

    added 3d object to scene

--- components/remove-self-if-oculusgo.js
+++ components/remove-self-if-oculusgo.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent('remove-self-if-oculusgo', {
     tick: function () {
-      if(AFRAME.utils.device.isOculusGo())
+      if(AFRAME.utils.device.isOculusGo() || AFRAME.utils.device.checkHasPositionalTracking())
         this.el.parentNode.removeChild(this.el);
     },
   });
